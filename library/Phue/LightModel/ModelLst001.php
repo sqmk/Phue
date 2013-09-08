403ecf49aabb1178067b0bcdc992840fd5e1aae8
Michael Squires <sqmk@php.net>
2013-09-07 21:08:43 -0700

    GitHub #57 - Add lightstrips model

---
+++ library/Phue/LightModel/ModelLst001.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Phue\LightModel;
+
+class ModelLst001 extends AbstractLightModel
+{
+	/**
+	 * Model id
+	 */
+	const MODEL_ID = 'LST001';
+
+	/**
+	 * Model name
+	 */
+	const MODEL_NAME = 'LightStrips';
+}
