81e2eaf0cdc6fc4d846a5ea240dc3a7832291ec9
Michael Squires <sqmk@php.net>
2013-09-07 21:09:04 -0700

    GitHub #57 - Add unknown model

---
+++ library/Phue/LightModel/ModelUnknown.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Phue\LightModel;
+
+class ModelUnknown extends AbstractLightModel
+{
+	/**
+	 * Model id
+	 */
+	const MODEL_ID = '';
+
+	/**
+	 * Model name
+	 */
+	const MODEL_NAME = 'Bloom';
+}
