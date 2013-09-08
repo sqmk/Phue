aafeb2b2c4340e7547561cc2ccc03f27d8b89f54
Michael Squires <sqmk@php.net>
2013-09-08 13:48:03 -0700

    GitHub #57 - Fix spacing

--- library/Phue/LightModel/LightModelFactory.php
+++ library/Phue/LightModel/LightModelFactory.php
@@ -7,24 +7,24 @@ namespace Phue\LightModel;
  */
 class LightModelFactory
 {
-	/**
-	 * Build a new light model from model id
-	 *
-	 * @param string $modelId Model id
-	 *
-	 * @return AbstractLightModel Light model
-	 */
-	public static function build($modelId)
-	{
-		$classNamePrefix      = __NAMESPACE__ . '\Model';
-		$classNameModelSuffix = ucfirst(strtolower($modelId));
+    /**
+     * Build a new light model from model id
+     *
+     * @param string $modelId Model id
+     *
+     * @return AbstractLightModel Light model
+     */
+    public static function build($modelId)
+    {
+        $classNamePrefix      = __NAMESPACE__ . '\Model';
+        $classNameModelSuffix = ucfirst(strtolower($modelId));
 
-		if (!class_exists($classNamePrefix . $classNameModelSuffix)) {
-			$classNameModelSuffix = 'Unknown';
-		}
+        if (!class_exists($classNamePrefix . $classNameModelSuffix)) {
+            $classNameModelSuffix = 'Unknown';
+        }
 
-		$finalClassName = $classNamePrefix . $classNameModelSuffix;
+        $finalClassName = $classNamePrefix . $classNameModelSuffix;
 
-		return new $finalClassName;
-	}
+        return new $finalClassName;
+    }
 }
--- library/Phue/LightModel/ModelUnknown.php
+++ library/Phue/LightModel/ModelUnknown.php
@@ -4,13 +4,13 @@ namespace Phue\LightModel;
 
 class ModelUnknown extends AbstractLightModel
 {
-	/**
-	 * Model id
-	 */
-	const MODEL_ID = '';
+    /**
+     * Model id
+     */
+    const MODEL_ID = '';
 
-	/**
-	 * Model name
-	 */
-	const MODEL_NAME = 'Unknown';
+    /**
+     * Model name
+     */
+    const MODEL_NAME = 'Unknown';
 }
