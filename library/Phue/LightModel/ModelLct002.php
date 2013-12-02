5e498f989e133dace0543bb4b84e48ceccc3759d
Michael Squires <sqmk@php.net>
2013-12-01 22:11:48 -0800

    GitHub #73 - Add BR30 bulb model

---
+++ library/Phue/LightModel/ModelLct002.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Phue\LightModel;
+
+/**
+ * BR30 model
+ */
+class ModelLct002 extends AbstractLightModel
+{
+    /**
+     * Model id
+     */
+    const MODEL_ID = 'LCT002';
+
+    /**
+     * Model name
+     */
+    const MODEL_NAME = 'BR30 Hue Bulbs';
+
+    /**
+     * Can retain state
+     */
+    const CAN_RETAIN_STATE = false;
+}
