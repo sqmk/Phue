8a0330cff01674300a3a1e87279296fce3625945
Michael Squires <sqmk@php.net>
2013-12-01 22:08:26 -0800

    GitHub #73 - Add GU10 bulb model

---
+++ library/Phue/LightModel/ModelLct003.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Phue\LightModel;
+
+/**
+ * Hue bulb model
+ */
+class ModelLct001 extends AbstractLightModel
+{
+    /**
+     * Model id
+     */
+    const MODEL_ID = 'LCT003';
+
+    /**
+     * Model name
+     */
+    const MODEL_NAME = 'GU10 Hue Bulb';
+}
