45578a29ee1b82818bd5da7b79beb5dd3f59fc06
Michael K. Squires <sqmk@php.net>
2014-12-11 21:30:54 -0800

    GitHub #87 - Add example for listing schedules

---
+++ examples/list-schedules.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Example: List schedules.
+ *
+ * Usage: HUE_HOST=127.0.0.1 HUE_USERNAME=1234567890 php list-lights.php
+ */
+
+require_once 'common.php';
+
+$client = new \Phue\Client($hueHost, $hueUsername);
+
+echo 'Listing schedules:', "\n";
+
+foreach ($client->getSchedules() as $schedule) {
+    echo "\t", "#{$schedule->getId()} - {$schedule->getName()}", "\n",
+        "\t\t", "Time scheduled: {$schedule->getTime()}", "\n",
+        "\t\t", "Method: {$schedule->getCommand()['method']}", "\n",
+        "\t\t", "Address: {$schedule->getCommand()['address']}", "\n",
+        "\t\t", "Body: ", json_encode($schedule->getCommand()['body']), "\n";
+}
