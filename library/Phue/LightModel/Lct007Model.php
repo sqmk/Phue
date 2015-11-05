f84961208ce48c7694bfb6707c104f0b57c8e09b
Kasper Hartwich <kasper@hartwich.net>
2015-11-05 22:41:20 +0100

    Added more lights.

---
+++ library/Phue/LightModel/Lct007Model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Phue: Philips Hue PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2012 Michael K. Squires
+ * @license   http://github.com/sqmk/Phue/wiki/License
+ */
+
+namespace Phue\LightModel;
+
+/**
+ * Hue Bulb V2
+ */
+class Lct007Model extends AbstractLightModel
+{
+    /**
+     * Model id
+     */
+    const MODEL_ID = 'LCT007';
+
+    /**
+     * Model name
+     */
+    const MODEL_NAME = 'Hue Bulb V2';
+}
