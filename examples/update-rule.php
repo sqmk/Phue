967ba1b1356573cacb02e449a8a34bb9aa7fa7bc
JustJofe <just.jofe@gmail.com>
2016-03-23 14:03:40 +0100

    Backport to PHP 5.3

--- examples/update-rule.php
+++ examples/update-rule.php
@@ -14,21 +14,40 @@ echo 'Updating test rule', "\n";
 // TODO $sensor = $client->getSensors()[2];
 $sensors = $client->getSensors();
 $sensor = $sensors[2];
-// TODO $rule = $client->getRules()[5];
-$rules = $client->getRules()
-$rule = $client->getRules()[5];
 
-$client->sendCommand(
-    (new \Phue\Command\UpdateRule($rule))
-        ->name('New name')
-        ->addCondition(
-            (new \Phue\Condition)
-                ->setSensorId($sensor)
-                ->setAttribute('lastupdated')
-                ->changed()
-        )
-        ->addAction(
-            (new \Phue\Command\SetGroupState(0))
-                ->brightness(200)
-        )
-);
+// TODO $rule = $client->getRules()[5];
+$rules = $client->getRules();
+$rule = $rules[5];
+
+// $client->sendCommand(
+//     (new \Phue\Command\UpdateRule($rule))
+//         ->name('New name')
+//         ->addCondition(
+//             (new \Phue\Condition)
+//                 ->setSensorId($sensor)
+//                 ->setAttribute('lastupdated')
+//                 ->changed()
+//         )
+//         ->addAction(
+//             (new \Phue\Command\SetGroupState(0))
+//                 ->brightness(200)
+//         )
+// );
+
+$x = new \Phue\Command\UpdateRule($rule);
+$y1 = new \Phue\Condition;
+$y2 = new \Phue\Command\SetGroupState(0);
+$z = $x
+	->name('New name')
+	->addCondition(
+		$y1
+			->setSensorId($sensor)
+			->setAttribute('lastupdated')
+			->changed()
+	)
+	->addAction(
+		$y2
+			->brightness(200)
+	);
+
+$client->sendCommand($z);
