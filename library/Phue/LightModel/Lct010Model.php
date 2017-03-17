abc00eec6e2573f2ce0bf77c8a8a01623ea2fe8f
Garrett W <garrettw87@gmail.com>
2017-03-17 04:18:22 +0000

    added some newer model bulbs

---
+++ library/Phue/LightModel/Lct010Model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Phue: Philips Hue PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2012 Michael K. Squires
+ * @license   http://github.com/sqmk/Phue/wiki/License
+ */
+namespace Phue\LightModel;
+
+/**
+ * Hue Bulb V3
+ */
+class Lct010Model extends AbstractLightModel
+{
+
+    /**
+     * Model id
+     */
+    const MODEL_ID = 'LCT010';
+
+    /**
+     * Model name
+     */
+    const MODEL_NAME = 'Hue Bulb V3';
+}
